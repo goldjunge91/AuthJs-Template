ab11358859556340aa73a074d49dbccb53e5ea82
Goldjunge91 <m.tozzi91@icloud.com>
2025-02-24 07:02:21 +0100

    feat: adding middleware and routes.ts files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,59 @@
-export { auth as middleware } from '@/auth';
+// export { auth as middleware } from './auth';
+import { NextResponse, NextRequest } from 'next/server';
+import { auth } from './auth';
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  authRoutes,
+  publicRoutes,
+  apiAuthPrefix,
+} from './routes';
+
+export default async function middleware(request: NextRequest) {
+  const { nextUrl } = request;
+  const ip = request.headers.get('x-forwarded-for') ?? '127.0.0.1';
+
+  // Check if the requested path starts with API auth prefix
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  const session = await auth();
+  const isLoggedIn = !!session;
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('request-ip', ip);
+    return NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+    return NextResponse.redirect(
+      new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    );
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+};
---
+++ src/routes.ts
@@ -0,0 +1,58 @@
+/**
+ * These routes are public and don't need authentication
+ * @type {string[]}
+ * */
+export const publicRoutes = [
+  '/',
+  '/new-verification',
+  '/onboarding',
+  '/settings',
+  '/login',
+  '/register',
+  '/reset-password',
+  '/new-password',
+  '/loginerror',
+  '/login/magic-link',
+  '/admin-example',
+  '/admin',
+  '/settings',
+  '/client',
+  '/server',
+  '/admin',
+  '/client',
+  '/settings',
+];
+
+/**
+ * These routes are used for authentication,
+ * redirect logged-in users to /settings
+ * @type {string[]}
+ * */
+export const authRoutes = [
+  '/onboarding',
+  '/register',
+  '/loginerror',
+  '/reset-password',
+  '/new-password',
+  '/login/magic-link',
+];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for api
+ * authentication purposes
+ * @type {string}
+ * */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * Default redirect path for logged-in users
+ * @type {string}
+ * */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
+
+/**
+ * Default Allowed Redirects from callbackUrl searchParams
+ * @type {string}
+ * */
+export const ALLOWED_REDIRECTS = ['/server', '/admin', '/client', '/settings'];
